45fa8946f2a0b37d7c92e215cdc2f35fba5ca5cc
MohammedTubji <tubji98@gmail.com>
2020-03-30 19:12:23 +0300

    Query Builder DB

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'TaskController@index');
+
+Route::get('task/{id}','TaskController@show');
+
+Route::post('store','TaskController@store');
